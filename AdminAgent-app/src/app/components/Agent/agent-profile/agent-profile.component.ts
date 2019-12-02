77f6f34dd6b1b986539ffe9c97212176123a58a7
Jay Kukadiya <jaykukadiya99@gmail.com>
2019-12-02 11:14:37 +0530

    inquiry status comp

--- AdminAgent-app/src/app/components/Agent/agent-profile/agent-profile.component.ts
+++ AdminAgent-app/src/app/components/Agent/agent-profile/agent-profile.component.ts
@@ -11,7 +11,14 @@ import { CountTotalService } from "../../../shared/Agent/count-total.service";
 })
 export class AgentProfileComponent implements OnInit {
   agentProfile: any = {
-    agencyAddress: '',
+    agencyAddress: {
+      addressline1: " ",
+      addressline2: " ",
+      area: " ",
+      pincode: " ",
+      city: "",
+      state: ""
+    },
     agentName: '',
     agencyName: '',
     agencyEmail: '',
@@ -20,23 +27,23 @@ export class AgentProfileComponent implements OnInit {
   };
   contactNumber = [];
   public imageAgency;
-  public totPackages:any;
-  public totInquiry:any;
+  public totPackages: any;
+  public totInquiry: any;
   public baseUri = "http://localhost:58030/Resources/";
-  public contacts:string;
+  public contacts: string;
   constructor(private router: Router,
-    private _agentService: AgentLoginService, private _packageService:PackageService, private _countTotalService:CountTotalService) { }
+    private _agentService: AgentLoginService, private _packageService: PackageService, private _countTotalService: CountTotalService) { }
 
   ngOnInit() {
     this.getAgent();
     this._countTotalService.getTotalCount().subscribe(
-      data=>{
+      data => {
         // console.log(data);
-        this.totPackages=data;
-        this.totPackages=this.totPackages.totalPackage;
-        this.totInquiry=data;
-        this.totInquiry=this.totInquiry.totalInquiry;
-      }, error=>{
+        this.totPackages = data;
+        this.totPackages = this.totPackages.totalPackage;
+        this.totInquiry = data;
+        this.totInquiry = this.totInquiry.totalInquiry;
+      }, error => {
         console.log(error);
       }
     )
@@ -46,8 +53,47 @@ export class AgentProfileComponent implements OnInit {
     this._agentService.getAgent().subscribe(
       data => {
         this.agentProfile = data;
-        this.contacts = this.agentProfile.contactMobile+'';
+        this.contacts = this.agentProfile.contactMobile + '';
         //console.log(data);
+
+        var jdata = JSON.parse(JSON.stringify(this.agentProfile));
+
+        console.log(jdata.agencyAddress);
+        if (jdata.agencyAddress === null) {
+          // console.log("null")
+          this.agentProfile.agencyAddress.addressLine1=""
+          this.agentProfile.agencyAddress.addressLine2=""
+          this.agentProfile.agencyAddress.area=""
+          this.agentProfile.agencyAddress.city=""
+          this.agentProfile.agencyAddress.state=""
+          this.agentProfile.agencyAddress.pincode=""
+        }
+        else {
+          if (jdata.agencyAddress.addressLine1 == null) {
+            // console.log("add1")
+            this.agentProfile.agencyAddress.addressLine1=""
+          }
+          if (jdata.agencyAddress.addressLine2 == null) {
+            // console.log("add2")
+            this.agentProfile.agencyAddress.addressLine2=""
+          }
+          if (jdata.agencyAddress.area == null) {
+            // console.log("area")
+            this.agentProfile.agencyAddress.area=""
+          }
+          if (jdata.agencyAddress.city == null) {
+            // console.log("city")
+            this.agentProfile.agencyAddress.city=""
+          }
+          if (jdata.agencyAddress.state == null) {
+            // console.log("state")
+            this.agentProfile.agencyAddress.state=""
+          }
+          if (jdata.agencyAddress.pincode == null) {
+            // console.log("pincode")
+            this.agentProfile.agencyAddress.pincode=""
+          }
+        }
       },
       err => {
         console.log(err);
@@ -61,16 +107,14 @@ export class AgentProfileComponent implements OnInit {
     this.agentProfile.contactMobile = this.contacts.split(",");
     let formsdata = new FormData();
 
-    if(typeof(this.imageAgency) === "undefined")
-    {
+    if (typeof (this.imageAgency) === "undefined") {
       console.log("image not");
-    }    
-    else
-    {
-      formsdata.append("agencyImage",this.imageAgency[0],this.imageAgency[0]["name"]);      
     }
-    formsdata.append("data",JSON.stringify(this.agentProfile));
-    
+    else {
+      formsdata.append("agencyImage", this.imageAgency[0], this.imageAgency[0]["name"]);
+    }
+    formsdata.append("data", JSON.stringify(this.agentProfile));
+
     this._agentService.updateAgent(formsdata).subscribe(
       data => {
         window.alert("Your profile is updated");
